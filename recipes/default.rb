73a921ff31b357a4be6ff7948d5899baf0ff5a89
RichardWigley <richard.wigley@gmail.com>
2014-05-23 07:31:12 +0000

    moved certificate manage into bcs_logstash

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,14 +8,3 @@
 #
 include_recipe 'certificate::manage_by_attributes'
 
-# user "deployer" do
-# end
-
-certificate_manage 'logstash_ssl' do
-  key_file 'ssl_key.key'
-  cert_file 'ssl_certificate.crt'
-  # owner "deployer"
-  # group "deployer"
-end
-
-
